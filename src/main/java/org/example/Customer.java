bded7b03afe6f7aaefbf712a6a264f584627d63e
micha <mimagra@gmx.net>
2023-05-09 15:15:10 +0200

    removed unnecessary ; in Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -8,13 +8,13 @@ class Customer {
     private final Vector<Rental> rentals = new Vector<>();
     public Customer (String newname){
         name = newname;
-    };
+    }
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
     public String getName (){
         return name;
-    };
+    }
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
