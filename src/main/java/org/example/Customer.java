b2e7db8b27e02131178454cdd572be16ca043293
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-09 14:48:53 +0200

    Move amount-calculation to Rental-class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -22,40 +22,19 @@ public class Customer {
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         for (Rental rental : rentals) {
-            double thisAmount;
-            //determine amounts for each line
-            thisAmount = amountFor(rental);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
             if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints ++;
             //show figures for this rental
-            result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(thisAmount).append("\n");
-            totalAmount += thisAmount;
+            result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(rental.getCharge()).append("\n");
+            totalAmount += rental.getCharge();
         }
         //add footer lines
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
-    }
-
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
-            case Movie.CHILDRENS -> {
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-            }
-        }
-        return thisAmount;
     }
 
 }
