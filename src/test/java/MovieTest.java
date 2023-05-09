1ef5730c83d7964757c24be3f5979acfdcd7cd97
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-09 14:20:23 +0200

    Add Tests

---
+++ src/test/java/MovieTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.example.*;
+
+
+public class MovieTest {
+
+    private Movie movie;
+
+    @Before
+    public void setUp() {
+        movie = new Movie("Movie Title", 0);
+    }
+
+    @Test
+    public void getPriceCode() {
+        assertEquals(0, movie.getPriceCode());
+    }
+
+    @Test
+    public void getTitle() {
+        assertEquals("Movie Title", movie.getTitle());
+    }
+}
