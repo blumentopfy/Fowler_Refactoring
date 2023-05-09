a54fc531a583c5f6e99c127699f4c9189950cb4a
GloriaSelina.Landsberger <gloriaselina.landsberger@dm.de>
2023-05-09 15:42:16 +0200

    Moved the calculation for amount in for loop to rental

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -22,37 +22,15 @@ class Customer {
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         for (Rental rental : rentals) {
-            double amount;
-            //determine amounts for each line
-            amount = amountFor(rental);
             frequentRenterPoints += rental.getFrequentRenterPoints();
             //show figures for this rental
             result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(String.valueOf(amount)).append("\n");
-            totalAmount += amount;
+            totalAmount += rental.getCharge();
         }
         //add footer lines
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
     }
-
-    private double amountFor(Rental each) {
-        double amount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                amount += 2;
-                if (each.getDaysRented() > 2)
-                    amount += (each.getDaysRented() - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> amount += each.getDaysRented() * 3;
-            case Movie.CHILDRENS -> {
-                amount += 1.5;
-                if (each.getDaysRented() > 3)
-                    amount += (each.getDaysRented() - 3) * 1.5;
-            }
-        }
-        return amount;
-    }
-
 }
     
--- src/main/java/org/example/Rental.java
+++ src/main/java/org/example/Rental.java
@@ -13,6 +13,23 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+    public double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR -> {
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+            }
+            case Movie.NEW_RELEASE -> result += getDaysRented() * 3;
+            case Movie.CHILDRENS -> {
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+            }
+        }
+        return result;
+    }
     public int getFrequentRenterPoints() {
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
             return 2;
