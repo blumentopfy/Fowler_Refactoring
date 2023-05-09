417efb9032263385fd4b0a2689c40e97e8accfe3
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-09 15:05:57 +0200

    Remove temps to remove loop-executions

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -16,21 +16,35 @@ public class Customer {
         return name;
     }
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         for (Rental rental : rentals) {
-            frequentRenterPoints += rental.getFrequentRenterPoints();
             //show figures for this rental
             result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(rental.getCharge()).append("\n");
-            totalAmount += rental.getCharge();
         }
-        //add footer lines
-        result.append("Amount owed is ").append(totalAmount).append("\n");
-        result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+        result.append("Amount owed is ").append(getTotalCharge()).append("\n");
+        result.append("You earned ").append(getTotalFrequentRenterPoints()).append(" frequent renter points");
         return result.toString();
+    }
+    private double getTotalCharge() {
+        double charge = 0;
+
+        for (Rental rental : rentals) {
+            charge +=  rental.getCharge();
+        }
+
+        return charge;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int points = 0;
+
+        for (Rental rental : rentals) {
+            points += rental.getFrequentRenterPoints();
+        }
+
+        return points;
     }
 }
     
