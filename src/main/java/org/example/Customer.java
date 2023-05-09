20ff267bc50b74009c47ee5a2569a4261d070886
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-09 14:31:14 +0200

    Remove explicit toString()-call when it's already implicitly done

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -18,7 +18,7 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
@@ -45,19 +45,17 @@ public class Customer {
     private double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+            case Movie.REGULAR -> {
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
+            }
+            case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
+            case Movie.CHILDRENS -> {
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
+            }
         }
         return thisAmount;
     }
